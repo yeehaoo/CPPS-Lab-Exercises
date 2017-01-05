da6db345905d26a5d7a9dc1cb9b0853f202ec977
Yee Hao <yeehaofake@hotmail.com>
2017-01-05 22:13:07 +0800

    Update main.c

--- WaterViewer2/main.c
+++ WaterViewer2/main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
+void showMenu();
+void average();
+void waterConsumption();
+void averageDifference();
+void quarterlyTotal();
+void lowestConsumption();
+void fiveRoomConsumption();
 
 void main(void) {
 	char menuSelection;
-	showMenu();
-	scanf("%c", &menuSelection);
-	fflush(stdin);
+	do
+	{
+		showMenu();
+		scanf_s(" %c", &menuSelection);
+		fflush(stdin);
+		switch (menuSelection) {
+		case 'a':
+		case 'A':
+			average();
+			break;
+		case 'b':
+		case 'B':
+			waterConsumption();
+			break;
+		case 'c':
+		case 'C':
+			averageDifference();
+			break;
+		case 'd':
+		case 'D':
+			quarterlyTotal();
+			break;
+		case 'e':
+		case 'E':
+			lowestConsumption();
+			break;
+		case 'f':
+		case 'F':
+			fiveRoomConsumption();
+			break;
+		case 'q':
+		case 'Q':
+			menuSelection = 'Q';
+			break;
+		default:
+			printf("You have selected an invalid function. Please try again.\n");
+		}//switch
+	} while (menuSelection!='Q');//do()
 }//main()
