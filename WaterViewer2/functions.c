f974f507b367f8dee43c706a2f5cb7779fe90167
Yee Hao <yeehaofake@hotmail.com>
2017-01-18 22:33:06 +0800

    Update functions.c

--- WaterViewer2/functions.c
+++ WaterViewer2/functions.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-extern float oneRoom[];
-extern float twoRoom[];
-extern float threeRoom[];
-extern float fourRoom[];
-extern float fiveRoom[];
+
+extern float household[][12];
 
 int selectedHousehold = 0;
-int x = 0;
+int j = 0;
+int k = 0;
+int i = 0;
 float totalConsumption = 0.0;
 float averageConsumption = 0.0;
 int selectedMonth = 0;
+float averageDiff = 0.0;
 
 void showMenu(void)
 {
@@ -25,117 +25,103 @@ void showMenu(void)
 
 void average(void)
 {
-	
 	do {
-		printf("Please select the household by entering 1 2 3 4 or 5. Enter 9 to go back to the main menu.");
-		scanf_s("%d", &selectedHousehold);
-		switch (selectedHousehold) {
-		case 1:
-			for (x = 0; x < 13; x++) {
-				totalConsumption += oneRoom[x];
-			}//for()
-			averageConsumption = totalConsumption / 12.0;
-			printf("The average water consumption for HDB 1-Room is %.2f.\n", averageConsumption);
-			break;
-		case 2:
-			for (x = 0; x < 13; x++) {
-				totalConsumption += twoRoom[x];
-			}//for()
-			averageConsumption = totalConsumption / 12.0;
-			printf("The average water consumption for HDB 2-Room is %.2f.\n", averageConsumption);
-			break;
-		case 3:
-			for (x = 0; x < 13; x++) {
-				totalConsumption += threeRoom[x];
-			}//for()
-			averageConsumption = totalConsumption / 12.0;
-			printf("The average water consumption for HDB 3-Room is %.2f.\n", averageConsumption);
-			break;
-		case 4:
-			for (x = 0; x < 13; x++) {
-				totalConsumption += fourRoom[x];
-			}//for()
-			averageConsumption = totalConsumption / 12.0;
-			printf("The average water consumption for HDB 4-Room is %.2f.\n", averageConsumption);
-			break;
-		case 5:
-			for (x = 0; x < 13; x++) {
-				totalConsumption += fiveRoom[x];
-			}//for()
-			averageConsumption = totalConsumption / 12.0;
-			printf("The average water consumption for HDB 5-Room is %.2f.\n", averageConsumption);
-			break;
-		case 9:
-			selectedHousehold = 9;
-			break;
-		default:
-			printf("You have made an invalid selection. Please select the HDB household by entering 1, 2, 3, 4 or 5. Enter 9 to return to the main menu.\n");
+	printf("Please select the household by entering its numerical value. Enter 9 to return to main menu.\n");
+	scanf_s("%d", &selectedHousehold);
+	if (selectedHousehold == 9)continue;
+	for (j = 0; j < 12; j++) {
+			totalConsumption += household[selectedHousehold - 1][j];
+		}
+	averageConsumption = totalConsumption / 12.0;
+	printf("The average consumption of %droom household is %.2f.\n\n\n", selectedHousehold, averageConsumption);
+	totalConsumption = 0;
+	averageConsumption = 0;
+	} while (selectedHousehold != 9);
+}
 
-		}//switch()
-		printf("\n\n");
-	} while (selectedHousehold != 9);//do()
-}//average()
-
-void waterConsumption(void)
-{
+void waterConsumption(void) {
 	do {
-		printf("Please select the household by entering 1, 2, 3, 4 or 5. Enter 9 to return to the main menu.");
+		printf("Please select the household by entering its numerical value. Enter 9 to return to main menu.\n");
 		scanf_s("%d", &selectedHousehold);
-		switch (selectedHousehold) {
-		case 1:
-			printf("Please select the month by entering its numerical value (eg. Jan is 01, Feb is 02)");
-			scanf_s("%d", &selectedMonth);
-			printf("The water consumption of %d-room HDB in the month %d is %.1f.\n\n",selectedHousehold,selectedMonth,oneRoom[selectedMonth-1]);
-			break;
-		case 2:
-			printf("Please select the month by entering its numerical value (eg. Jan is 01, Feb is 02)");
-			scanf_s("%d", &selectedMonth);
-			printf("The water consumption of %d-room HDB in the month %d is %.1f.\n\n", selectedHousehold, selectedMonth, twoRoom[selectedMonth - 1]);
+		if (selectedHousehold == 9)continue;
+		printf("Please select the month by entering its numerical value.\n");
+		scanf_s("%d", &selectedMonth);
+		printf("The water consumption in the month %d in %droom household is %.2f.\n\n\n", selectedMonth, selectedHousehold, household[selectedHousehold - 1][selectedMonth - 1]);
+	} while (selectedHousehold != 9);
+}
 
-			break;
-		case 3:
-			printf("Please select the month by entering its numerical value (eg. Jan is 01, Feb is 02)");
-			scanf_s("%d", &selectedMonth);
-			printf("The water consumption of %d-room HDB in the month %d is %.1f.\n", selectedHousehold, selectedMonth, threeRoom[selectedMonth - 1]);
+void averageDifference(void) {
+	do {
+		printf("Please select the month by entering its numerical value. Enter 0 to return to main menu.");
+		scanf_s("%d", &selectedMonth);
+		if (selectedMonth == 0)continue;
+		for (j = 0; j < 5; j++) {
+			totalConsumption += household[j][selectedMonth - 1];
+		}
+		averageConsumption = totalConsumption / 5.0;
+		printf("The average consumption of all households in month %d is %.2f.\n", selectedMonth, averageConsumption);
+		for (j = 0; j < 5; j++) {
+			averageDiff = household[j][selectedMonth] - averageConsumption;
+			printf("The difference of water consumption between %droom household and average for month %d is %.2f.\n", j + 1, selectedMonth, averageDiff);
+		}
+		averageConsumption = 0.0;
+		totalConsumption = 0.0;
+	} while (selectedMonth != 0);
+}
+void quarterlyTotal(void) {
+	//Display quarterly total water consumption of a selected HDB household
+	do {
+		printf("Please select the household by entering its numerical value. Enter 9 to return to main menu.\n");
+		scanf_s("%d", &selectedHousehold);
+		if (selectedHousehold == 9)continue;
+		k = 0;
+		do {
+			i = k * 4;
+			j = i;
+			for (j = i; j < i+3; j++) {
+				totalConsumption += household[selectedHousehold - 1][j];
+			}
+			printf("The water consumption for quarter %d in %droom household is %.2f\n", k+1, selectedHousehold, totalConsumption);
+			totalConsumption = 0;
+			k++;
+		} while (k<4);
+		puts("\n");
+	} while (selectedHousehold != 9);
+}
+void lowestConsumption(void) {
+	int lowestMonth[] = { 0,0,0,0,0 };
+	int	minHousehold = 0;
+	int minMonth = 0;
+	float min = 999.0;
+	float lowest[] = { 999.0,999.0,999.0,999.0,999.0 };
+	for (j = 0; j < 5; j++) {
+		for (k = 0; k < 12; k++) {
+			if (household[j][k] < lowest[j]) {
+				lowest[j] = household[j][k];
+				lowestMonth[j] = k;
+			}
+		}
+		if (lowest[j] < min) {
+			min = lowest[j];
+			minMonth = lowestMonth[j];
+			minHousehold = j;
+		}
+	}
+	printf("The lowest consumption is %.2f, which occurred in month %d and in %droom household.\n\n", min,minMonth+1,minHousehold+1);
+}
 
-			break;
-		case 4:
-			printf("Please select the month by entering its numerical value (eg. Jan is 01, Feb is 02)");
-			scanf_s("%d", &selectedMonth);
-			printf("The water consumption of %d-room HDB in the month %d is %.1f.\n\n", selectedHousehold, selectedMonth, fourRoom[selectedMonth - 1]);
-
-			break;
-		case 5:
-			printf("Please select the month by entering its numerical value (eg. Jan is 01, Feb is 02)");
-			scanf_s("%d", &selectedMonth);
-			printf("The water consumption of %d-room HDB in the month %d is %.1f.\n\n", selectedHousehold, selectedMonth, fiveRoom[selectedMonth - 1]);
-
-			break;
-		case 9:
-			selectedHousehold = 9;
-			break;
-		default:
-			printf("You have made an invalid selection. Please select the HDB household by enter 1, 2, 3, 4 or 5. Enter 9 to return to the main menu.\n");
-		}//switch()
-	} while (selectedHousehold != 9);//do()
-}//waterConsumption()
-
-void averageDifference(void)
-{
-
-}//averageDifference
-
-void quarterlyTotal(void)
-{
-
-}//quarterlyTotal()
-
-void lowestConsumption(void)
-{
-
-}//lowestConsumption()
-
-void fiveRoomConsumption()
-{
-
-}//fiveRoomConsumption()
+void fiveRoomConsumption(void) {
+	float increaseAmt[11];
+	float highestIncrease = 0;
+	int highestMonth = 0;
+	for (j = 0; j < 11; j++) {
+		increaseAmt[j] = household[4][j + 1] - household[4][j];
+	}
+	for (k = 0; k < 11; k++) {
+		if (increaseAmt[k] > highestIncrease) {
+			highestIncrease = increaseAmt[k];
+			highestMonth = k+1;
+		}
+	}
+	printf("The highest increase is %.2f, which occurred from month %d to month %d.\n\n", highestIncrease,highestMonth++.,highestMonth);
+}
